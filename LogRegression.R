a0e36a5a1127e2d0a34ae0f422d98a0b5f3d6fd7
Roman Khalkechev <romario@romario-osx3.local>
2015-12-08 11:04:36 +0300

    Added momentum technique for gradient descent

--- LogRegression.R
+++ LogRegression.R
@@ -34,20 +34,22 @@ LogRegressionCost <- function(theta, x, y, regularizationRate=0.0) {
 
 LogRegressionTrain <- function(x, y, stochastic=FALSE, batchSize=300,
                                regularizationRate=0.0, learningRate=1.0,
-                               maxNumIters=10000, verbouse=TRUE) {
+                               maxNumIters=1000, momentum=0.0, verbose=TRUE) {
   # Implementation of logistic regression train algorithm using gradient descent
   # Args:
   #   x: matrix of features
   #   y: vector of labels
-  #   stochastic: use stochastic gradient descent
-  #   batchSize: size of train batch for stochastic gradient descent
-  #   regularizationRate: regularization rate
-  #   learningRate: learning rate
-  #   maxNumIters: maximal numbers of gradient descent iterations
+  #   stochastic: use stochastic gradient descent or not (default FALSE)
+  #   batchSize: size of batch for stochastic gradient descent (default 300)
+  #   regularizationRate: regularization rate (default 0.0)
+  #   learningRate: learning rate (default 1.0)
+  #   maxNumIters: maximal numbers of gradient descent iterations (default 1000)
+  #   momentum: momentum technique coefficient (default 0.0)
+  #   verbose: print additional details of training (default TRUE)
   #
   # Returns:
   #   vector of parameters theta
-  kThreshold <- 0.1e-10
+  kErrorsThreshold <- 0.1e-10
   bias <- rep(1, nrow(x))
   x <- cbind(bias, x)
   theta <- matrix(0.0, ncol(x), 1)
@@ -55,29 +57,67 @@ LogRegressionTrain <- function(x, y, stochastic=FALSE, batchSize=300,
     batchSize <- nrow(x)
   }
   indices <- sample(1:nrow(x), batchSize)
-  previousCostResult <- LogRegressionCost(theta, x[indices,],
-                                          as.matrix(y[indices]),
-                                          regularizationRate)
-  costResult <- previousCostResult
-  for(iter in 1:maxNumIters) {
-    step <- 0.8 * previousCostResult$gradient + 0.2 * costResult$gradient
-    theta <- theta - learningRate * step
-    indices <- sample(1:nrow(x), batchSize)
-    costResult <- LogRegressionCost(theta, x[indices,], as.matrix(y[indices]),
+  previousCost <- LogRegressionCost(theta, x[indices,], as.matrix(y[indices]),
                                     regularizationRate)
-    if (abs(costResult$error - previousCostResult$error) < kThreshold) {
+  previosStep <- previousCost$gradient
+  cost <- previousCost
+  for(iter in 1:maxNumIters) {
+    step <- - (momentum * previosStep + (1 - momentum) * cost$gradient)
+    theta <- theta + learningRate * step
+    indices <- sample(1:nrow(x), batchSize)
+    cost <- LogRegressionCost(theta, x[indices,], as.matrix(y[indices]),
+                              regularizationRate)
+    if (abs(cost$error - previousCost$error) < kErrorsThreshold) {
       break
     }
-    if (previousCostResult$error < costResult$error) {
+    if (previousCost$error < cost$error) {
       learningRate <- learningRate / 1.5
     }
-    if (verbouse && iter %% 1000 == 0) {
-      message <- paste(c("Current error:", costResult$error), collapse=" ")
+    if (verbose && iter %% 500 == 0) {
+      message <- paste(c("Current error:", cost$error), collapse=" ")
       write(message, stderr())
     }
-    previousCostResult <- costResult
+    previousCost <- cost
+    previosStep <- step
   }
   return(theta)
+}
+
+OneVsAllLogRegressionTrain <- function(x, y, numLabels, stochastic=FALSE,
+                                       batchSize=300, regularizationRate=0.0,
+                                       learningRate=1.0, maxNumIters=1000,
+                                       momentum=0.0, verbose=TRUE) {
+  # Trains multiple logistic regression classifiers and returns numLabels
+  # classifiers in a matrix allTheta, where the i-th row of allTheta
+  # corresponds to the classifier for label i
+  # Args:
+  #   x: matrix of features
+  #   y: vector of labels
+  #   numLabels: number of different labels
+  #   stochastic: to use stochastic gradient descent (default FALSE)
+  #   batchSize: size of batch for stochastic gradient descent (default 300)
+  #   regularizationRate: regularization rate (default 0.0)
+  #   learningRate: learning rate (default 1.0)
+  #   maxNumIters: maximal numbers of gradient descent iterations (default 1000)
+  #   momentum: momentum technique coefficient (default 0.0)
+  #   verbose: print additional details of training (default TRUE)
+  #
+  # Returns:
+  #   matrix of parameters theta for every classifier
+  allTheta <- matrix(0.0, numLabels, ncol(x) + 1)
+  for (lableCounter in 1:numLabels) {
+    if (verbose) {
+      message <- paste(c("Training classifier number", lableCounter),
+                       collapse=" ")
+      write(message, stderr())
+    }
+    allTheta[lableCounter,] <- LogRegressionTrain(x, (y == lableCounter),
+                                                  stochastic, batchSize,
+                                                  regularizationRate,
+                                                  learningRate, maxNumIters,
+                                                  momentum, verbose)
+  }
+  return (allTheta)
 }
 
 ################################################################################
@@ -92,8 +132,8 @@ LogRegressionPredictProba <- function(theta, x) {
   #   vector of probabilities
   bias <- rep(1, nrow(x))
   x <- cbind(bias, x)
-  sigmoid <- Sigmoid(x %*% theta)
-  return (sigmoid)
+  probabilities <- Sigmoid(x %*% theta)
+  return (probabilities)
 }
 
 LogRegressionPredict <- function(theta, x, threshold=0.5) {
@@ -105,7 +145,7 @@ LogRegressionPredict <- function(theta, x, threshold=0.5) {
   #
   # Returns:
   #   vector of labels
-  probabilities <- LogRegressionPredictProba(theta, x)
+  probabilities <- LogRegressionPredictProba(theta, x) # проще написать можно?
   labels <- rep(0, nrow(x))
   for (objCounter in 1:nrow(x)) {
     if (probabilities[objCounter] > threshold) {
@@ -116,41 +156,6 @@ LogRegressionPredict <- function(theta, x, threshold=0.5) {
 }
 
 ################################################################################
-
-OneVsAllLogRegressionTrain <- function(x, y, numLabels, stochastic=FALSE,
-                                       batchSize=300, regularizationRate=0.0,
-                                       learningRate=1.0, maxNumIters=10000,
-                                       verbouse=TRUE) {
-  # Trains multiple logistic regression classifiers and returns numLabels
-  # classifiers in a matrix allTheta, where the i-th row of allTheta 
-  # corresponds to the classifier for label i
-  # Args:
-  #   x: matrix of features
-  #   y: vector of labels
-  #   stochastic: to use stochastic gradient descent
-  #   batchSize: size of train batch for stochastic gradient descent
-  #   numLabels: number of different labels
-  #   regularizationRate: regularization rate
-  #   learningRate: learning rate
-  #   maxNumIters: maximal numbers of gradient descent iterations
-  #
-  # Returns:
-  #   matrix of parameters theta for every classifier
-  allTheta <- matrix(0.0, numLabels, ncol(x) + 1)
-  for (lableCounter in 1:numLabels) {
-    if (verbouse) {
-      message <- paste(c("Training classifier number", lableCounter),
-                       collapse=" ")
-      write(message, stderr())
-    }
-    allTheta[lableCounter,] <- LogRegressionTrain(x, (y == lableCounter),
-                                                  stochastic, batchSize,
-                                                  regularizationRate,
-                                                  learningRate, maxNumIters,
-                                                  verbouse)
-  }
-  return (allTheta)
-}
 
 MultiLabelLogRegressionPredictProba <- function(allTheta, x) {
   # Predicts probability of every label, using marix of parameters allTheta
